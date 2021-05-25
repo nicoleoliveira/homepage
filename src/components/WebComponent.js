bdf8bcb67a41fcc3a400287e5049e20673a1dc44
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-05-25 15:36:35 -0300

    correção para o SSR

--- src/components/WebComponent.js
+++ src/components/WebComponent.js
@@ -1,25 +1,19 @@
-// import '@webcomponents/custom-elements';
-// import '@webcomponents/webcomponentsjs';
 
-// import '@animaliads/ani-button';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 import React from 'react';
 
 export default class WebComponentReact extends React.Component {
     render() {
-        return <div>Olá !</div>;
-        // return <div>Olá <ani-button>button</ani-button>!</div>;
+        return (
+            <BrowserOnly
+                fallback={<div>The fallback content to display on prerendering</div>}>
+                {() => {
+                    require('@webcomponents/custom-elements');
+                    require('@animaliads/ani-button');
+                    return <div>Olá <ani-button>button</ani-button>!</div>;
+                }}
+            </BrowserOnly>
+        );
     }
 }
-
-// export const MyComponent = () => {
-//     return (
-//       <BrowserOnly
-//         fallback={<div>The fallback content to display on prerendering</div>}>
-//         {() => {
-//         require('@animaliads/ani-button');
-//         <ani-button>button</ani-button>
-//         }}
-//       </BrowserOnly>
-//     );
-//   };
